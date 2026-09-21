54f6329efdd5fffe5ae57bd141e819a1ea018e86
elaiza joy dominguez <elaizajoydominguez@gmail.com>
2026-09-21 21:13:14 +0800

    Added student name and section to console output

--- GitLabActivity/Program.cs
+++ GitLabActivity/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Git Lab Activity - VS Code");
+Console.WriteLine("Name : Elaiza Joy C. Dominguez");
+Console.WriteLine("Section: BSCS 2-4");
